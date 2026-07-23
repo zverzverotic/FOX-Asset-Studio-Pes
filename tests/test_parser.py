72e50c6406973e918fad609c91cae271852f7cd2
zverzverotic <zver.zverotic@gmail.com>
2026-07-23 16:12:12 +0200

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,37 +4,62 @@ from pathlib import Path
 from fox_core.parser import FoxParser
 
 
-class TestFoxParser(unittest.TestCase):
+class ParserTests(unittest.TestCase):
 
     def setUp(self):
+
         self.parser = FoxParser()
 
-        # Promeni ovu putanju na svoj JSON
-        self.sample = Path("samples/tv_template_st081_basic.json")
+        self.sample = Path(
+            "samples/tv_template_st081_basic.json"
+        )
 
     def test_load(self):
 
         asset = self.parser.load(self.sample)
 
-        self.assertGreaterEqual(asset.version, 1)
+        self.assertEqual(
+            asset.version,
+            2
+        )
 
-    def test_has_templates(self):
-
-        asset = self.parser.load(self.sample)
-
-        self.assertGreater(asset.template_count, 0)
-
-    def test_has_textures(self):
+    def test_templates_exist(self):
 
         asset = self.parser.load(self.sample)
 
-        self.assertGreater(asset.texture_count, 0)
+        expected = {
+
+            "match_score_small",
+
+            "score_large",
+
+            "match_score_large",
+
+            "match_small",
+
+            "score_small",
+
+            "match_large",
+
+        }
+
+        self.assertEqual(
+            set(asset.templates.keys()),
+            expected
+        )
+
+    def test_textures(self):
+
+        asset = self.parser.load(self.sample)
+
+        self.assertEqual(
+            asset.texture_count,
+            3
+        )
 
     def test_texture_indexes(self):
 
         asset = self.parser.load(self.sample)
-
-        texture_count = asset.texture_count
 
         for template in asset.templates.values():
 
@@ -47,7 +72,7 @@ class TestFoxParser(unittest.TestCase):
 
                 self.assertLess(
                     element.texture_index,
-                    texture_count
+                    asset.texture_count
                 )
 
 
